282c4145445d0df954aefd5fd53d6e92fea12d70
wjspark <weijuSpark@outlook.com>
2026-05-06 11:22:59 +0800

    add all site files

---
+++ config.js
@@ -0,0 +1,8 @@
+window.SITE_CONFIG = {
+  domain: "https://caaca.top",
+  siteName: "H5 Free GameBox",
+  description:
+    "Play 200+ free H5 games online. No download, no login required. Puzzle, action, sports and more!",
+  keywords: "free h5 games, online games, browser games, puzzle games, casual games, play free",
+  ogImage: "https://caaca.top/webp/2048-remastered.webp",
+};
